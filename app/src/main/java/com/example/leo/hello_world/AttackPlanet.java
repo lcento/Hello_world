712da60db1ff6faca012cfa51149577b8ce661cd
lcento <lcento@gmail.com>
2015-12-14 15:50:01 +0100

    Update 141215 15:49

--- app/src/main/java/com/example/leo/hello_world/AttackPlanet.java
+++ app/src/main/java/com/example/leo/hello_world/AttackPlanet.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by leo on 04/12/15.
@@ -14,6 +15,34 @@ public class AttackPlanet extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attack);
+        ImageButton bombButton = (ImageButton)findViewById(R.id.bombButton);
+        bombButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AttackPlanet.this, "Bombs Away!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton invadeButton = (ImageButton)findViewById(R.id.invadeButton);
+        invadeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AttackPlanet.this, "Troops Sent!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton infectButton = (ImageButton)findViewById(R.id.infectButton);
+        infectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AttackPlanet.this, "Virus Spread!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ImageButton laserButton = (ImageButton)findViewById(R.id.laserButton);
+        laserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AttackPlanet.this, "Laser Fired!", Toast.LENGTH_SHORT).show();
+            }
+        });
         ImageButton exitButton = (ImageButton)findViewById(R.id.exitButton);
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +50,7 @@ public class AttackPlanet extends Activity {
                 finish();
             }
         });
+
     }
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_X) {
